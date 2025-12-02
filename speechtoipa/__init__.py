839eb11b44fe28745856b0998d5947ed58817478
audiophrases <emonfor3@xtec.cat>
2025-12-03 00:40:40 +0100

    Delete speechtoipa directory

--- speechtoipa/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Speech-to-IPA prototype package."""
-
-__all__ = ["transcribe_audio_to_ipa", "TranscriptionResult", "SegmentResult"]
-
-from .pipeline import transcribe_audio_to_ipa, TranscriptionResult, SegmentResult
